753e0a9a95602cccaee5dc9f075d6c89aaab3952
ilyaperelygin <tort21345y@gmail.com>
2024-06-07 00:12:47 +0300

    fix vite plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import viteLogo from "../../../../../../vite.svg";
-
 import reactLogo from "./assets/react.svg";
 
 import "./App.css";
@@ -12,9 +10,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
