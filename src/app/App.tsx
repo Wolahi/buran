108ce130cca958e02ca6e8b5f66d7a4d0ba12074
Nikita Klyukoit <klukoyt@dterra.ru>
2024-06-10 22:03:12 +0300

    add styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,5 @@
-import { useState } from "react";
-
-import "./styles/App.scss";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div className="app">qwe</div>;
 }
 
 export default App;
