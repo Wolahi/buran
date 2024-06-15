e1335ed370632908921bdd1c68279b4412c533bc
Nikita Klyukoit <klukoyt@dterra.ru>
2024-06-15 17:15:17 +0300

    upd hero & small fix

--- src/widgets/Hero/hero.tsx
+++ src/widgets/Hero/hero.tsx
@@ -1,22 +1,45 @@
-import { Button } from "antd";
-
 import style from "./hero.module.scss";
 
 import buran_mp4 from "@/assets/hero/buran_vid.mp4";
 import buran_webm from "@/assets/hero/buran_vid.webm";
+import { Link } from "react-router-dom";
 
 const Hero = () => (
-  <div className={style.root}>
-    <video autoPlay loop muted playsInline className={style.hero}>
-      <source src={buran_webm} type="video/webm" />
-      <source src={buran_mp4} type="video/mp4" />
+  <div className={style.hero}>
+    <video
+      autoPlay
+      loop
+      muted
+      playsInline
+      className={style.hero__video}
+    >
+      <source
+        src={buran_webm}
+        type="video/webm"
+      />
+      <source
+        src={buran_mp4}
+        type="video/mp4"
+      />
     </video>
-    <div className={style.bg} />
-    <div className={style.container}>
-      <span className={style.text}>БУРАН - ВМЕСТЕ К ПОБЕДЕ!</span>
-      <Button className={style.button} href="#archive">
-        Посмотреть сезоны
-      </Button>
+    <div className={style.hero__container}>
+      <span className={style.hero__text}>
+        БУРАН&nbsp;-&nbsp;ВМЕСТЕ К&nbsp;ПОБЕДЕ!
+      </span>
+      <div className={style.hero__buttons}>
+        <Link
+          className={style.hero__button}
+          to="/#sectionSeasons"
+        >
+          Посмотреть список сезонов
+        </Link>
+        <Link
+          className={style.hero__button}
+          to="/commands"
+        >
+          Посмотреть команду
+        </Link>
+      </div>
     </div>
   </div>
 );
