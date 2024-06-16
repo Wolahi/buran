5d69fcfd85e5682d9877967961ddd041bfcdafab
Brodyaga44 <vdon_hiznyakov_dmitriy@mail.ru>
2024-06-16 15:55:29 +0300

    player info addded

--- src/entity/CommandsPlayer/CommandsPlayer.tsx
+++ src/entity/CommandsPlayer/CommandsPlayer.tsx
@@ -1,26 +1,25 @@
+import { Link } from "react-router-dom";
+
 import { CommandsPlayerProps } from "./interfaces/CommandsProps";
 
 import styles from "./CommandsPlayer.module.scss";
 
+import { translateRolePlayer } from "@/shared/config/translateRoleTeam.ts";
 import PlayerImage from "@/shared/ui/PlayerImage/ui/PlayerImage";
-import { Link } from "react-router-dom";
 
-const CommandsPlayer = ({ player }: CommandsPlayerProps) => {
-  return (
-    <Link
-      to={`player/${player.id}`}
-      className={styles.cpage__container}
-    >
-      <div className={styles.slider}>
-        <PlayerImage player={player} />
+const CommandsPlayer = ({ player }: CommandsPlayerProps) => (
+  <Link to={`player/${player.id}`} className={styles.cpage__container}>
+    <div className={styles.slider}>
+      <PlayerImage player={player} />
+    </div>
+    <div className={styles.cpage__items}>
+      <div className={styles.cpage__name}>{player?.name}</div>
+      <div className={styles.cpage__number}>{player?.number}</div>
+      <div className={styles.cpage__role}>
+        {translateRolePlayer[player?.type]}
       </div>
-      <div className={styles.cpage__items}>
-        <div className={styles.cpage__name}>{player?.name}</div>
-        <div className={styles.cpage__number}>{player?.number}</div>
-        <div className={styles.cpage__role}>{player?.type}</div>
-      </div>
-    </Link>
-  );
-};
+    </div>
+  </Link>
+);
 
 export default CommandsPlayer;
--- src/widgets/PlayerDetail/PlayerDetail.tsx
+++ src/widgets/PlayerDetail/PlayerDetail.tsx
@@ -1,24 +1,32 @@
 import styles from "./PlayerDetail.module.scss";
 
 import { IPlayerSimpleOutput } from "@/shared/config/interfaces/IPlayerSimpleOutput";
+import { translateRolePlayer } from "@/shared/config/translateRoleTeam.ts";
 
 type PlayerDetailProps = {
   player?: IPlayerSimpleOutput;
 };
 
-const PlayerDetail = ({ player }: PlayerDetailProps) => {
-  return (
-    <div className={styles.news}>
-      <div>{player?.name}</div>
-      <div>{player?.biography}</div>
-      <div>{player?.number}</div>
-      <div>{player?.type}</div>
-      <img
-        src={player?.url}
-        alt=""
-      />
+const PlayerDetail = ({ player }: PlayerDetailProps) => (
+  <div className={styles.pdetail}>
+    <div className={styles.pdetail__container}>
+      <div className={styles.pdetail__items}>
+        <div className={styles.pdetail__foto}>
+          <img src={player?.url} alt="" />
+        </div>
+        <div className={styles.pdetail__info}>
+          <div className={styles.pdetail__title}>
+            <div className={styles.pdetail__name}>{player?.name}</div>
+            <div className={styles.pdetail__number}>{player?.number}</div>
+          </div>
+          <div className={styles.pdetail__role}>
+            {translateRolePlayer[player?.type]}
+          </div>
+          <div className={styles.pdetail__bio}>{player?.biography}</div>
+        </div>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default PlayerDetail;
