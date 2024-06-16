1c43bfa1735757f13425f1e21e65164d7b848bd3
Brodyaga44 <vdon_hiznyakov_dmitriy@mail.ru>
2024-06-16 04:11:04 +0300

    bredik 2

--- src/pages/CommandsPage/CommandsPage.tsx
+++ src/pages/CommandsPage/CommandsPage.tsx
@@ -1,3 +1,25 @@
-const CommandsPage = () => <div>CommandsPage</div>;
+import style from "./commandspage.module.scss";
+import styles from "@/entity/Match/ui/Match.module.scss";
+
+import PlayerImage from "@/shared/ui/PlayerImage/ui/PlayerImage.tsx";
+
+const CommandsPage = () => (
+  <div className={style.cpage}>
+    <div className={style.cpage__container}>
+      <div className={style.cpage__items}>
+        <div className={styles.item}>
+          <div className={styles.slider}>
+            <PlayerImage player={player} />
+          </div>
+        </div>
+        <div>
+          <div className={style.cpage__name}>Иван Иванович Иванов</div>
+          <div className={style.cpage__number}>52</div>
+          <div>role</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default CommandsPage;
